a3d87d957de909b0aea3a00e619718b8c73ac3ac
Pedro Sampaio <pamms@ua.pt>
2025-04-02 13:38:54 +0100

    Edição da pagina de contactos

--- resources/views/contact/_header.blade.php
+++ resources/views/contact/_header.blade.php
@@ -2,10 +2,9 @@
 
 <div class="text-center mb-10">
     <h2 class="text-2xl md:text-3xl font-bold text-gray-800 mb-2">
-      Get in Touch With Us
+      Entre em contacto connosco!
     </h2>
     <p class="text-gray-600 text-sm md:text-base">
-      If you have any questions or doubts, feel free to contact us using the form.
+      Se tiver alguma questão ou duvida, sinta-se a vontade para nos contactar usando o formulário.
     </p>
   </div>
-  
